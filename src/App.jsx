645166f57703260cc9149a6d9868d774975a7b0d
kuzpugonka <6599255@bk.ru>
2024-10-11 12:48:27 +0300

    add Home-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import { Header } from './components/Header/Header'
+import { Home } from './pages/Home/Home'
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<><Header /></>}></Route>
+        <Route path='/' element={<><Home /></>}></Route>
         <Route></Route>
       </Routes>
       
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+import { Header } from "../../components/Header/Header";
+import { Menu } from "../../components/Menu/Menu";
+import "./Home.css";
+
+export const Home = () => {
+  return (
+    <main className="home">
+      <div className="container">
+        <Header />
+        <div className="home__content">
+          <h1 className="home__title">дикие и домашние животные</h1>
+          <p className="home__cta">
+            Исследуй <span className="home__cta_color">удивительный </span>
+             мир&nbsp;животных вместе с&nbsp;нами! Нажми на кнопку и&nbsp;начни&nbsp;своё&nbsp;приключение!          
+          </p>
+          <Menu />
+        </div>
+      </div>
+    </main>
+  );
+};

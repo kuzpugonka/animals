798d8b5312870666330e4609e1ad5f00eb87bf91
kuzpugonka <6599255@bk.ru>
2024-10-12 22:16:47 +0300

    add task

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,18 +3,21 @@ import "./Header.css";
 
 export const Header = () => {
   return (
-    <header className="header">
-      <div className="container header__container">
-        <Link to="/" className="header__logo">
-          <img
-            src="./src/components/Header/logo.png"
-            srcSet="./src/components/Header/logo2x.png 2x"
-          />
-        </Link>
-        <button className="header__burger hidden">
-          <img src="./src/components/Header/burger.svg" />
-        </button>
-      </div>
-    </header>
+    <>
+      <header className="header">
+        <div className="container header__container">
+          <Link to="/" className="header__logo">
+            <img
+              src="./src/components/Header/logo.png"
+              srcSet="./src/components/Header/logo2x.png 2x"
+            />
+          </Link>
+          <button className="header__burger hidden">
+            <img src="./src/components/Header/burger.svg" />
+          </button>
+        </div>
+        TODO ВЫПАДАЮЩЕЕ МЕНЮ ИЗ БУРГЕРА
+      </header>
+    </>
   );
 };

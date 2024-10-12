51b75434ef96047cdd8d1be48b74480314931cfc
kuzpugonka <6599255@bk.ru>
2024-10-12 21:53:52 +0300

    edit  error-pade

---
+++ src/pages/Error/Error.jsx
@@ -0,0 +1,13 @@
+import "./Error.css";
+
+export const Error404 = () => {
+  return (
+    <>
+      <div className="error">
+        {/* TODO ДОБАВИТЬ ВЁРСТКУ */}
+
+        <img className="error__img" src="./bg.png" />
+      </div>
+    </>
+  );
+};

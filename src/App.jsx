274aa30ede1eacd3e964301ceee8e5c4485fb97e
kuzpugonka <6599255@bk.ru>
2024-10-12 21:54:23 +0300

    edit route

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,43 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import { Home } from './pages/Home/Home'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Home } from "./pages/Home/Home";
+import { Animals } from "./pages/Animals/Animals";
+import { Animal } from "./pages/Animal/animal";
 
 function App() {
-
   return (
     <>
       <Routes>
-        <Route path='/' element={<><Home /></>}></Route>
-        <Route></Route>
+        <Route
+          path="/"
+          element={
+            <>
+              <Home />
+            </>
+          }
+          errorElement={
+            <><Error/></>
+          }
+        ></Route>
+        <Route
+          path="/animals"
+          element={
+            <>
+              <Animals />
+            </>
+          }
+        ></Route>
+        <Route
+          path="/animal"
+          element={
+            <>
+              <Animal />
+            </>
+          }
+        ></Route>
       </Routes>
-      
-    
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;

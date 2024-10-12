6a39fa82769e82388db3c12338e0acee6c798fb1
kuzpugonka <6599255@bk.ru>
2024-10-12 21:38:45 +0300

    add animal-page

---
+++ src/pages/Animal/Animal.jsx
@@ -0,0 +1,9 @@
+import { Header } from '../../components/Header/Header';
+import './Animal.css'
+
+export const Animal = () => {
+  return <main className='animal'>
+  <Header/>
+  TODO ANIMAL-PAGE
+  </main>;
+};

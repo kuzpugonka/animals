c2f4cf247692c637dc581b052a640515d7b0649a
kuzpugonka <6599255@bk.ru>
2024-10-12 21:27:08 +0300

    add menu

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import "./Menu.css";
+
+export const Menu = () => {
+  return (
+    <nav className="menu">
+      <Link to={"./about"} className="menu__link">
+        О&nbsp;сайте
+      </Link>
+      <Link to={"./gallery"} className="menu__link">
+        Галерея
+      </Link>
+      <Link to={"./animals"} className="menu__link menu__link_btn">
+        Животные
+      </Link>
+    </nav>
+  );
+};

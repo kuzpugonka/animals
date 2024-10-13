dc3d537c05e566feb8a166d33179d863b38fd351
kuzpugonka <6599255@bk.ru>
2024-10-13 17:09:58 +0300

    edit style

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -2,22 +2,26 @@ import { Link } from "react-router-dom";
 import "./Error.css";
 
 export const Error404 = () => {
-	return (
-		<>
-			<div className="error">
-				{/* TODO ДОБАВИТЬ ВЁРСТКУ */}
-				<div className="container">
-					<div className="error__row">
-						<h2 className="error__title">404</h2>
-						<img className="error__img" src="./src/pages/Error/bg.png" />
-						<p className="error__description">
-							Запрашиваемая Вами страница не существует или была
-							удалена.<br /><span>Вернитесь</span> на главную страницу
-						</p>
-						<Link className="menu__link menu__link_btn menu__link_btn" to="/">На главную</Link>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="error">
+        <div className="container">
+          <div className="error__row">
+            <h2 className="error__title">404</h2>
+            <img className="error__img" src="./src/pages/Error/bg.png" />
+            <p className="error__description">
+              Запрашиваемая Вами страница не&nbsp;существует
+              или&nbsp;была&nbsp;удалена.
+              <br />
+              <span className="error__description_accent">Вернитесь</span>{" "}
+              на&nbsp;главную страницу.
+            </p>
+            <Link className="error__link" to="/">
+              На&nbsp;главную
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };

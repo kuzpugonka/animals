f1155164d02137d09579bd28275b6bb1d34903da
kuzpugonka <6599255@bk.ru>
2024-10-20 23:48:04 +0300

    add gallery

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -1,10 +1,14 @@
-import { Header } from "../../components/Header/Header"
-import './Gallery.css'
+import "react-image-gallery/styles/css/image-gallery.css";
+import "./Gallery.css";
+import { animalsArray } from "../../data/animals.js";
+import ImageGallery from "react-image-gallery";
 
 export const Gallery = () => {
-  return(
-    <>
-      <Header/>
-    </>
-  )
-}
+  return (
+    <main className="gallery">
+      <div className="container">
+        <ImageGallery items={animalsArray} />;
+      </div>
+    </main>
+  );
+};

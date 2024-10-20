8894d4b7247406895c809cd1400d7cb2e7b82488
kuzpugonka <6599255@bk.ru>
2024-10-20 19:11:06 +0300

    add mobile

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import "./Home.css";
 export const Home = () => {
   return (
     <main className="home">
-      <div className="container">
+      <div className="container home__container">
         <div className="home__content">
           <h1 className="home__title">дикие и домашние животные</h1>
           <p className="home__cta">
@@ -12,6 +12,9 @@ export const Home = () => {
              мир&nbsp;животных вместе с&nbsp;нами! Нажми на кнопку и&nbsp;начни&nbsp;своё&nbsp;приключение!          
           </p>
           <Menu />
+        </div>
+        <div className="home__img">
+          <img className="home__img-bg" src="./img/Home-page/home-bg.png" srcSet="./img/Home-page/home-bg2x.png"/>
         </div>
       </div>
     </main>

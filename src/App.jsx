37fc3a71fd9014172abda0ba2a9e2409b69bccd8
kuzpugonka <6599255@bk.ru>
2024-10-21 23:57:03 +0300

    del header for home-page

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,6 @@ function App() {
           path="/"
           element={
             <>
-              <Header />
               <Home />
             </>
           }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,17 +4,30 @@ import "./Home.css";
 export const Home = () => {
   return (
     <main className="home">
-      <div className="container home__container">
-        <div className="home__content">
-          <h1 className="home__title">дикие и домашние животные</h1>
-          <p className="home__cta">
-            Исследуй <span className="home__cta_color">удивительный </span>
-             мир&nbsp;животных вместе с&nbsp;нами! Нажми на кнопку и&nbsp;начни&nbsp;своё&nbsp;приключение!          
-          </p>
-          <Menu />
+      <div className="container">
+        <div className="home__logo">
+          <img
+            src="./img/header-component/logo.png"
+            srcSet="./img/header-component/logo2x.png 2x"
+          />
         </div>
-        <div className="home__img">
-          <img className="home__img-bg" src="./img/Home-page/home-bg.png" srcSet="./img/Home-page/home-bg2x.png"/>
+        <div className="home__container">
+          <div className="home__content">
+            <h1 className="home__title">дикие и домашние животные</h1>
+            <p className="home__cta">
+              Исследуй <span className="home__cta_color">удивительный </span>
+              мир&nbsp;животных вместе с&nbsp;нами! Нажми на кнопку
+              и&nbsp;начни&nbsp;своё&nbsp;приключение!
+            </p>
+            <Menu />
+          </div>
+          <div className="home__img">
+            <img
+              className="home__img-bg"
+              src="./img/Home-page/home-bg.png"
+              srcSet="./img/Home-page/home-bg2x.png"
+            />
+          </div>
         </div>
       </div>
     </main>

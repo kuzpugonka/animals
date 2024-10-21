056d87671ed5ed962d934b71d5d43fbc6d561030
kuzpugonka <6599255@bk.ru>
2024-10-21 21:12:34 +0300

    edit import

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
-import { Animals } from "./pages/Animals/Animals";
-import { Error404 } from "./pages/Error/Error";
 import { About } from "./pages/About/About";
 import { Gallery } from "./pages/Gallery/Gallery";
+import { Animals } from "./pages/Animals/Animals";
+import { Animal } from "./pages/Animal/Animal";
+import { Error404 } from "./pages/Error/Error";
 import { Header } from "./components/Header/Header";
 import { Search } from "./components/Search/Search";
 import { Footer } from "./components/Footer/Footer";
-import { Animal } from "./pages/Animal/Animal";
 
 function App() {
   return (

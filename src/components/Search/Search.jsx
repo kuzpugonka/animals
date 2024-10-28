6ce7ab799a71d08f243c0db4d1689c99bad5a25f
kuzpugonka <6599255@bk.ru>
2024-10-28 23:25:06 +0300

    add id for input

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -7,7 +7,7 @@ export const Search = () => {
       <section className="search">
         <div className="container">
           <form className="search__form">
-            <input className="search__form-input" placeholder="Введи название животного"/>
+            <input className="search__form-input" placeholder="Введи название животного" id="search" name="search"/>
             <button className="search__form-btn">
               <span className="search__form-btn_text">Найти</span>
               <img className="search__form-btn_icon" src="./img/icons/search-icon.svg"/>

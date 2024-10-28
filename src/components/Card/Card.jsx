e7f4d2bb01f86a8021ba24e8470eaede56bcf9f5
kuzpugonka <6599255@bk.ru>
2024-10-28 23:21:04 +0300

    fixed card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,25 +1,21 @@
 import { Link } from "react-router-dom";
 import "./Card.css";
 
-export const Card = ({ id, title, liter,  img }) => {
+export const Card = ({ id, title, liter, img, category }) => {
   return (
     <article className="card">
-    <Link to={`/animal/${id}`} className="card-link">
+      <Link to={`/animal/${id}`} className="card-link">
+        <div className="card__info">
+          <p className="card__liter">{liter}</p>
 
-      <div className="card__info">        
-        <p className="card__liter">{liter}</p>      
-        
-        <div  className="card__img-wrap">
-        <img
-          className="card__img"
-          src={img}
-          alt={title}
-        />
+          <div className="card__img-wrap">
+            <img className="card__img" src={img} alt={title} />
+          </div>
         </div>
-      </div> 
 
-      <h3 className="card__title">{title}</h3>
-    </Link>
+        <h3 className="card__title">{title}</h3>
+        <p className="card__category">{category}</p>
+      </Link>
     </article>
   );
 };

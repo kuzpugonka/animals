627f7ca8ad8e4b7ed1d82372070f3e54a8d4475a
kuzpugonka <6599255@bk.ru>
2024-10-28 23:22:50 +0300

    add page-catalog

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -1,18 +1,49 @@
+import { animalsArray } from "../../data/animals";
+import { Card } from "../../components/Card/Card";
+import { Side } from "../../components/Side/Side";
+import { animalsCategory } from "../../data/animalsCategory";
 import "./Catalog.css";
-
+// TODO разобраться с выпадающими card__desc
 export const Catalog = () => {
+  const handleClick = () => {
+    const desc = document.querySelector(".side__desc");
+    desc.style.display = "flex";
+    desc.add.className = "active"
+  };
+
   return (
-    <main className="catalog">
-      <section>
+    <main className="main">
+      <section className="catalog">
         <div className="container">
-          <p>
-            Животные&nbsp;&mdash; это удивительные создания, которые населяют
-            нашу планету. Они живут на&nbsp;суше, в&nbsp;воде
-            и&nbsp;в&nbsp;воздухе. Среди них есть большие и&nbsp;маленькие,
-            хищники и&nbsp;травоядные, домашние и&nbsp;дикие. Животные
-            отличаются друг от&nbsp;друга внешним видом, образом жизни
-            и&nbsp;поведением.
-          </p>{" "}
+          <h2 className="catalog__title">Каталог</h2>
+          <div className="side-wrap">
+            <div className="side">
+              {animalsCategory.map((item) => (
+                <button className="side__btn" key={item.id} onClick={handleClick}>{item.categoryRu}</button>
+              ))}
+            </div>
+          {animalsCategory.map((item) => (
+            <Side
+              key={item.id}
+              img={item.img}
+              text={item.description}
+              title={item.categoryRu}
+              category={item.category}
+            />
+          ))}
+        </div>
+
+        <div className="card-list">
+        {animalsArray.map(item=>(
+          <Card 
+            key={item.id}
+            title={item.title}
+            liter={item.liter}
+            img={item.original}
+            category={item.categoryRu}
+          />
+        ))}
+        </div>
         </div>
       </section>
     </main>

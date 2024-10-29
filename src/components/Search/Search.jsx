5f0a7062ca969f613b7d845960310951a0ad352d
kuzpugonka <6599255@bk.ru>
2024-10-29 14:18:21 +0300

    add aria-label

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -10,7 +10,7 @@ export const Search = () => {
             <input className="search__form-input" placeholder="Введи название животного" id="search" name="search"/>
             <button className="search__form-btn">
               <span className="search__form-btn_text">Найти</span>
-              <img className="search__form-btn_icon" src="./img/icons/search-icon.svg"/>
+              <img className="search__form-btn_icon" src="./img/icons/search-icon.svg" alt="search" aria-label="Кнопка поиска с изображением лупы"/>
             </button>
           </form>
         </div>
